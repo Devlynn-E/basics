b8d479b523c0fc50a90536c2a1ac8d2e1e6a775a
Devlynn-E <141595269+Devlynn-E@users.noreply.github.com>
2023-08-07 13:27:10 +1200

    Add files via upload

---
+++ 02_variables.py
@@ -0,0 +1,12 @@
+# code to learn about variables
+
+# variables can be a string
+greeting = "ello guvna"
+
+# they can also be numbers
+fave_num = 37
+
+# below is a constant (the name is in UPPERCASE)
+PI = 3.14159
+
+num_2 = 2
